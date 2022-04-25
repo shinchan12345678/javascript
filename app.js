fbce19ab5d52dcde98b7797c2f8c8933c5be1a91
shinchan12345678 <102851643+shinchan12345678@users.noreply.github.com>
2022-04-25 14:08:54 +0900

    Start

---
+++ app.js
@@ -0,0 +1,14 @@
+// let hello="Hello World";
+// alert(hello);
+
+// let orange=100;
+// let apple=120;
+
+// if(orange<apple){
+//   alert("みかんはリンゴより安い")
+// }else if(orange==apple){
+//   alert("みかんとりんごの値段は同じ")
+// }else {
+//   alert("リンゴはみかんより安い")
+// }
+
