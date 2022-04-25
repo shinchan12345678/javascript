22ae8cd1d631ff8985fd8ffae99c78296d8ce468
shinchan12345678 <102851643+shinchan12345678@users.noreply.github.com>
2022-04-25 14:24:27 +0900

    [Finish]5-3

--- app.js
+++ app.js
@@ -12,3 +12,35 @@
 //   alert("リンゴはみかんより安い")
 // }
 
+// let max=100;
+// let num=1;
+// let count=0;
+
+// // while(num<max){
+// //   num=num*2;
+// //   count=count+1;
+// // }
+
+// do{
+//   num=num*2;
+//   count=count+1;
+// }while(num<max);
+
+// alert("2をかけ続けて"+max+"を越えるのに必要だった回数は"+count+"回です")
+
+// let i;
+// let num=0;
+
+// for(i=1;i<11;i++){
+//   num=num+i;
+// }
+
+// alert("1から10まで足し算した結果は"+num+"です");
+
+let int_1=2;
+let int_2=6;
+
+alert(int_2+int_1);
+alert(int_2-int_1);
+alert(int_2*int_1);
+alert(int_2/int_1);
