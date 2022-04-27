eef4d44f9a1ad8f34b206d5d32dd7772d8c31765
shinchan12345678 <102851643+shinchan12345678@users.noreply.github.com>
2022-04-27 17:13:02 +0900

    [Finish]5-8

---
+++ SlideShow-Lesson/js/script.js
@@ -0,0 +1,12 @@
+$(".theTarget").skippr({
+  transition: 'fade',
+  speed: 1000,
+  easing: 'easeOutQuart',
+  nvType: 'block',
+  chidrenElementType: 'div',
+  arrows: true,
+  autoPlay: false,
+  autoPlayDuration: 5000,
+  keyboardOnAlways: true,
+  heidePrevious: false
+});
--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -43,3 +43,28 @@
 //     $('.box1').removeClass('box1-ext');
 //   });
 // });
+
+// $(function(){
+//   $('.box1').on('click',function(){
+//     $(this).slideUp();
+//   });
+
+  // $('.bg2').on('click',function(){
+  //   $('.bg2').slideUp();
+  // });
+
+  // $('.bg3').on('click',function(){
+  //   $('.bg3').slideUp();
+  // });
+
+  // $('.bg4').on('click',function(){
+  //   $('.bg4').slideUp();
+  // })
+// });
+
+
+$(function(){
+  $('button').on('click',function(){
+    $('ul').children().css('color','red');
+  });
+});
